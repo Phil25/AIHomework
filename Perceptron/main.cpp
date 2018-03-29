d5f1c50514e06115527442537c70cd4fa229f55e
Phil25 <Phil25@ProtonMail.com>
2018-03-29 13:32:07 +0200

    User examples

--- Perceptron/main.cpp
+++ Perceptron/main.cpp
@@ -9,25 +9,22 @@
 // number of coordinates in the data set
 #define COORD_NUM 4
 
+// structure for iris
 struct point{
 	bool is_virginica;
 	double coords[COORD_NUM];
 };
 
-std::vector<point> train_data;
-std::vector<point> test_data;
-
+// helper function declarations
 void read_data(std::string file, std::vector<point>& vec);
+point parse_line(std::string);
 std::string get_iris(bool);
 std::string get_coords(double[COORD_NUM]);
 void print3col(int col_dist, std::string, std::string, std::string);
-
-template <typename T>
-T get_user_num(const T min, const T max);
+template <typename T> T get_user_num(const T min, const T max);
 
 // training function
 void train_perc(perc& p, std::vector<point>& data){
-
 	// iterate through training data
 	auto end = data.end();
 	for(auto it = data.begin(); it != end; it++){
@@ -35,7 +32,6 @@ void train_perc(perc& p, std::vector<point>& data){
 		// tune the result
 		bool result = p.get(it->coords);
 		p.tune(it->coords, it->is_virginica, result);
-
 	}
 }
 
@@ -43,6 +39,7 @@ void test_perc(perc& p, std::vector<point>& data){
 	print3col(25, "COORDINATES", "CORRECT", "PREDICTED");
 	std::cout << std::endl;
 
+	// iterate through the training set
 	int correct = 0, all = 0;
 	auto end = data.end();
 	for(auto it = data.begin(); it != end; it++){
@@ -65,31 +62,27 @@ void test_perc(perc& p, std::vector<point>& data){
 	std::cout << "Accuracy: " << accuracy << '%' << std::endl;
 }
 
-point parse_line(std::string line){
-	point p;
-	std::istringstream iss(line);
-	std::string data;
-
-	// read coordinates
-	int i = 0;
-	while(std::getline(iss, data, ',') && i < COORD_NUM)
-		p.coords[i++] = stod(data);
-
-	// read class
-	std::getline(iss, data);
-	p.is_virginica = data.size() == 14;	
-
-	return p;
+void input_loop(perc& p){
+	double test_coords[COORD_NUM];
+	while(1){
+		std::cout << std::endl << "Enter coordinates: " << std::endl;
+		int i = 0;
+		while(i < COORD_NUM)
+			std::cin >> test_coords[i++];
+		std::cout << "Prediction: " << get_iris(p.get(test_coords)) << std::endl;
+	}
 }
 
 int main(){
+	std::vector<point> train_data;
+	std::vector<point> test_data;
 
 	// input the learning parameter
-	std::cout << "Provide learning parameter (0 < a < 1): ";
+	std::cout << "Learning parameter (0 < a < 1): ";
 	double learning_param = get_user_num<double>(0, 1);
 
 	// input number of iterations
-	std::cout << "Provide number of iterations for training: ";
+	std::cout << "Number of training iterations: ";
 	int training_iterations = get_user_num<int>(0, 999999);
 
 	// create perceptron class
@@ -104,6 +97,9 @@ int main(){
 	read_data("test.txt", test_data);
 	test_perc(p, test_data);
 
+	// run the user test input loop
+	input_loop(p);
+
 	return 0;
 }
 
@@ -114,6 +110,23 @@ inline void read_data(std::string file, std::vector<point>& vec){
 	std::string data;
 	while(std::getline(fs, data))
 		vec.push_back(parse_line(data));
+}
+
+inline point parse_line(std::string line){
+	point p;
+	std::istringstream iss(line);
+	std::string data;
+
+	// read coordinates
+	int i = 0;
+	while(std::getline(iss, data, ',') && i < COORD_NUM)
+		p.coords[i++] = stod(data);
+
+	// read class
+	std::getline(iss, data);
+	p.is_virginica = data.size() == 14;	
+
+	return p;
 }
 
 inline std::string get_iris(bool is_virginica){
